6855f8fe9cc8ae7254d0ec3ed77039e51a61eb0a
Gabriel Griffin Silva <programadorcodigomaestro@gmail.com>
2023-09-06 10:14:06 -0600

    Primer commit Proyecto

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+let nombreMaestro = "Jose Gabriel";
+let apellidoMaestro = "Griffin Silva";
+let edadMaestro = 30;
+
+var dataMaestro = document.getElementById("dataMaestro");
+
+dataMaestro.innerHTML = `
+Mi nombre es: ${nombreMaestro} ${apellidoMaestro}, tengo ${edadMaestro} años de edad.
+`;
+
+let nombreAlumnoUno = "Pedro Alberto"; 
+let apellidoAlumnoUno = "Lima Morales"; 
+let edadAlumnoUno = 22;
+
+var dataAlumnoUno = document.getElementById("dataAlumnoUno");
+
+dataAlumnoUno.innerHTML = `
+Mi nombre es: ${nombreAlumnoUno} ${apellidoAlumnoUno}, tengo ${edadAlumnoUno} años de edad.
+`;
+
+// COMIENZO DE CODIGO ALUMNOS
